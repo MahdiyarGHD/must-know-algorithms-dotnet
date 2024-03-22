0174ca7f85cb6a0a0ac00246c9f2e24a9ae532a6
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2024-03-22 04:26:54 +0330

    feat: Added bubble sort

---
+++ Bubble-Sort-Algorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arrayToSort = [6, 2, 4, 8, 9, 7, 12, 13];
+BubbleSort(arrayToSort);
+
+Console.WriteLine(string.Join(", ", arrayToSort));
+
+
+static void BubbleSort(int[] arrayToSort)
+{
+    int sortedItems;
+    while (true)
+    {
+        sortedItems = 0;
+        for (int i = 0; i < arrayToSort.Length - 1; i++)
+            if (arrayToSort[i + 1] < arrayToSort[i])
+            {
+                Swap(arrayToSort, i + 1, i);
+                sortedItems++;
+            }
+
+        if (sortedItems == 0)
+            break;
+    }
+}
+
+
+static void Swap(int[] arr, int i, int j)
+{
+    (arr[j], arr[i]) = (arr[i], arr[j]);
+}
